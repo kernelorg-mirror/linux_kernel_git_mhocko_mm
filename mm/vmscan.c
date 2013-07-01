bd43ac7a08f536425abab35b793d6f4ae8e039c6
Michal Hocko <mhocko@suse.cz>
2013-07-01 11:12:44 +0200

    Reverted "mm: vmscan: avoid direct reclaim scanning at maximum priority"
    This reverts 07482ccefa0c90bdb222490c0d5e3030e3df3d8b
    
    Patch has been dropped from mmotm tree with reason: withdrawn

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2368,14 +2368,12 @@ static unsigned long do_try_to_free_pages(struct zonelist *zonelist,
 	struct zone *zone;
 	unsigned long writeback_threshold;
 	bool aborted_reclaim;
-	int min_scan_priority = 1;
 
 	delayacct_freepages_start();
 
 	if (global_reclaim(sc))
 		count_vm_event(ALLOCSTALL);
 
-rescan:
 	do {
 		vmpressure_prio(sc->gfp_mask, sc->target_mem_cgroup,
 				sc->priority);
@@ -2430,7 +2428,7 @@ rescan:
 						WB_REASON_TRY_TO_FREE_PAGES);
 			sc->may_writepage = 1;
 		}
-	} while (--sc->priority >= min_scan_priority);
+	} while (--sc->priority >= 0);
 
 out:
 	delayacct_freepages_end();
@@ -2453,12 +2451,6 @@ out:
 	/* top priority shrink_zones still had more to do? don't OOM, then */
 	if (global_reclaim(sc) && !all_unreclaimable(zonelist, sc))
 		return 1;
-
-	/* If the page allocator is considering OOM, rescan at priority 0 */
-	if (min_scan_priority) {
-		min_scan_priority = 0;
-		goto rescan;
-	}
 
 	return 0;
 }
